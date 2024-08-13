1a936ab8d216b8cf4c9230e2ac226bfc6f3cc16e
holdping <1051633104@qq.com>
2024-08-13 16:31:00 +0800

    Added C-style extern "C" block to log.h for C++ compatibility.

--- log.h
+++ log.h
@@ -7,6 +7,9 @@
 #define LOG_INFO  1
 #define LOG_WARN  2
 #define LOG_ERROR 3
+#ifdef __cplusplus
+extern "C" {
+#endif
 typedef void (*log_callback_t)(uint8_t* buffer, uint8_t len); 
 void log_init(log_callback_t log);
 void log_message(int level, const char *fmt, ...);
@@ -14,5 +17,8 @@ void log_message(int level, const char *fmt, ...);
 #define log_info(fmt, ...) log_message(LOG_INFO, fmt, ##__VA_ARGS__)
 #define log_warn(fmt, ...) log_message(LOG_WARN, fmt, ##__VA_ARGS__)
 #define log_error(fmt, ...) log_message(LOG_ERROR, fmt, ##__VA_ARGS__)
+#ifdef __cplusplus
+}
+#endif
 
 #endif // LOG_H
